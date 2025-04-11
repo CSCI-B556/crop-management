a33ae2ebd2f7cdf6dd6d496f53d93f47a75bcc56
Hirata, Yudai <yuhirata@iu.edu>
2025-04-11 01:44:57 +0000

    greetings endpoint added

---
+++ src/main/java/edu/iu/yuhirata/farm/crop_management/controller/Home.java
@@ -0,0 +1,12 @@
+package edu.iu.yuhirata.farm.crop_management.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Home {
+    @GetMapping ("/")
+    public String greetings() {
+        return "hello from crop management system\n" + System.currentTimeMillis();
+    }
+}
